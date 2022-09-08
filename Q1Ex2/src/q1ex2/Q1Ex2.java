fd1f9d71ec06ed3eea7aba1d44297a5cd03acd30
Elijah-James <96934768+Elijah-James@users.noreply.github.com>
2022-09-08 11:24:53 +0800

    Add files via upload

---
+++ Q1Ex2/src/q1ex2/Q1Ex2.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q1ex2;
+
+/**
+ *
+ * @author MUON
+ */
+import java.util.Scanner;
+
+public class Q1Ex2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Higher or Lower! What will you do?");
+        System.out.println("-Start Game");
+        System.out.println("-Change Settings");
+        System.out.println("-End Application");
+        String menu = sc.nextLine();
+        
+        
+        //switch(menu){
+            //case "Start Game":
+                
+            //case "Change Settings":
+            
+            //case "End Application":
+                //System.exit(0);
+                
+            //default:
+                
+        }
+    }
+    
+}
